f9d50fa58bc63450025621de8ac4ac1477e69b8c
kagius <karl.agius@gmail.com>
2015-03-02 13:22:10 +0100

    Chapter 2: Player movement

---
+++ Assets/scripts/PlayerController.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var speed = 500.0;
+
+function FixedUpdate() {
+	var moveX = Input.GetAxis("Horizontal");
+	var moveZ = Input.GetAxis("Vertical");
+	
+	var movement = new Vector3(moveX, 0, moveZ);
+	rigidbody.AddForce(movement * speed * Time.deltaTime);
+}
