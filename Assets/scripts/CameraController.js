f9801c9bd01c0eae5169193a7b4a36c7d20725de
kagius <karl.agius@gmail.com>
2015-03-02 13:58:21 +0100

    Chapter 3: camera movement.

---
+++ Assets/scripts/CameraController.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+class CameraController extends MonoBehaviour {
+	private var offset: Vector3;
+	var player: GameObject;
+
+	function Start () {
+		offset = transform.position;
+	}
+
+	function LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
