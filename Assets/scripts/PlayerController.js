44514b330f43f2dbb81cb4ac03f76388d99f6d59
kagius <karl.agius@gmail.com>
2015-03-02 16:08:20 +0100

    Chapter 4: Pick up objects

--- Assets/scripts/PlayerController.js
+++ Assets/scripts/PlayerController.js
@@ -1,11 +1,13 @@
 ﻿#pragma strict
 
-var speed = 500.0;
+class PlayerController extends MonoBehaviour {
+	var speed = 500.0;
 
-function FixedUpdate() {
-	var moveX = Input.GetAxis("Horizontal");
-	var moveZ = Input.GetAxis("Vertical");
-	
-	var movement = new Vector3(moveX, 0, moveZ);
-	rigidbody.AddForce(movement * speed * Time.deltaTime);
+	function FixedUpdate() {
+		var moveX = Input.GetAxis("Horizontal");
+		var moveZ = Input.GetAxis("Vertical");
+		
+		var movement = new Vector3(moveX, 0, moveZ);
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
 }
---
+++ Assets/scripts/Rotator.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+class Rotator extends MonoBehaviour {
+	var rotation = new Vector3(15, 30, 45);
+	
+	function Update() {
+		transform.Rotate(rotation * Time.deltaTime);
+	}
+}
